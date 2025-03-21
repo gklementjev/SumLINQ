6a029dce96f06d174403ce8a5439141616f7e37b
Opilane TTHK <opilane@tthk.ee>
2025-03-21 11:23:42 +0200

    Init

---
+++ Sum LINQ/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 7, 12, 19, 24, 31, 42, 55, 63, 78 };
+
+            int sum = (from n in numbers
+                       where n % 2 != 0
+                       select n).Sum();
+
+            Console.WriteLine($"Paaritute arvude summa: {sum}");
+        }
+    }
+}
+
